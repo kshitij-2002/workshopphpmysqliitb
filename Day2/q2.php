8db1249083199a67b392e7ff1b8f7393c33c90cc
Kshitij <kshitijgowari2002@gmail.com>
2020-10-06 08:21:00 +0530

    Day2 commit

---
+++ Day2/q2.php
@@ -0,0 +1,10 @@
+<?php
+$x = array("one","two","three");
+echo "The array elements are:-";
+echo "<br>";
+for($i=0;$i<=2;$i++)
+{ 
+	echo $x[$i];
+	echo "<br>";
+}
+?>
---
+++ Day2/q4.php
@@ -0,0 +1,9 @@
+<?php
+function writename2($firstname)
+{
+echo "My name is Jai\n".$firstname;
+echo "<br>";
+echo "My brother's name is Ram\n".$firstname;
+}
+writename2("Sharma");
+?>
